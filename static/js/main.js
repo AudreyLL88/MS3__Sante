1160fcb8f980ac219a720898ce730a54f850b261
AudreyLL88 <a.lloancy@gmail.com>
2020-12-22 14:54:03 +0000

    Add comments on js file

--- static/js/main.js
+++ static/js/main.js
@@ -1,43 +1,50 @@
+/*jshint esversion: 6 */
+
+// Navbar const
+
 const navMenu = document.getElementById('nav-menu');
     toggleMenu = document.getElementById('nav-toggle');
     closeMenu = document.getElementById('nav-close');
 
-//Toggle Navbar on mobile
+// Toggle Navbar on mobile
+
 toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show');
-}); // Open navbar on click
+    navMenu.classList.toggle('show'); // Open navbar on click
+}); 
 
 closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show');
-});// Close navbar on click
+    navMenu.classList.remove('show'); // Close navbar on click
+});
+
+// If statement to prevent errors on the console 
 
 if (window.location.pathname==='/') {
 
-// Hero image animation on mouse move in index.html
-document.addEventListener('mousemove', move);
-function move(e){
-    this.querySelectorAll('.move').forEach(layer =>{
-        const speed = layer.getAttribute('data-speed');
-        const x = (window.innerWidth - e.pageX*speed)/120;
-        const y = (window.innerHeight - e.pageY*speed)/120;
+    // Hero image animation on mouse move in index.html
 
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    });
-}
+    document.addEventListener('mousemove', move);
 
-// GSAP Animation index.html hero elements
-gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30});
-gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30});
-gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30});
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30});
+    function move(e){
+        this.querySelectorAll('.move').forEach(layer =>{
+
+            const speed = layer.getAttribute('data-speed');
+            const x = (window.innerWidth - e.pageX*speed)/120; // Allow horizontal movement
+            const y = (window.innerHeight - e.pageY*speed)/120; // Allow vertical movement
+
+            layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        });
+    }
+
+    // GSAP Animation index.html hero elements
+
+    gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30}); // Delayed display hero title on page load
+    gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30}); // Delayed display hero description on page load
+    gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30}); //Delayed display hero button on page load
+    gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30}); // Delayed display hero image block on page load
 }
 
 
 // GSAP Animation navbar
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10});
-gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,});
 
-
-
-// Toggle Dropdown in Cocktails template
-$('.dropdown-toggle').dropdown();
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10}); // Delayed display navbar logo and toggler on page load
+gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,}); // Delayed display navbar links on page load
